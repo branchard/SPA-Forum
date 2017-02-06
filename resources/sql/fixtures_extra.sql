9fcfbc37e41f8e0203979acccd1be1ecc71bd8b6
branchard <benoit.branchard@gmail.com>
2017-02-06 17:46:57 +0100

    [SQL] Add ipsum in extra fixtures

--- resources/sql/fixtures_extra.sql
+++ resources/sql/fixtures_extra.sql
@@ -35,8 +35,10 @@ INSERT INTO thread
        WHERE username = 'admin'),
     idcategory = @category1;
 
+SET @ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
 INSERT INTO post
-    SET message = 'Message 1 du premier thread',
+    SET message = @ipsum,
     iduser = (
        SELECT iduser
        FROM user
@@ -45,7 +47,7 @@ INSERT INTO post
     idthread = @thread1;
 
 INSERT INTO post
-    SET message = 'Message 2 du premier thread',
+    SET message = @ipsum,
     iduser = (
        SELECT iduser
        FROM user
@@ -54,7 +56,7 @@ INSERT INTO post
     idthread = @thread1;
 
 INSERT INTO post
-    SET message = 'Message 3 du premier thread',
+    SET message = @ipsum,
     iduser = (
        SELECT iduser
        FROM user
