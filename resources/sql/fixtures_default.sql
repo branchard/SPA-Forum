a0313efec28eab20078f3a12dcd11a8a1c6399a0
branchard <benoit.branchard@gmail.com>
2017-02-09 08:52:31 +0100

    add some fixtures

--- resources/sql/fixtures_default.sql
+++ resources/sql/fixtures_default.sql
@@ -11,3 +11,10 @@ INSERT INTO user
     email = 'benoit@gmail.com',
     photo = 'https://yt3.ggpht.com/-fsMIgEUdBvI/AAAAAAAAAAI/AAAAAAAAAAA/zf-lh5Z07uM/s100-c-k-no-mo-rj-c0xffffff/photo.jpg',
     role = '2';
+
+INSERT INTO user
+    SET username = 'toto',
+    password = 'f2d81a260dea8a100dd517984e53c56a7523d96942a834b9cdc249bd4e8c7aa9', -- azerty
+    email = 'toto@gmail.com',
+    photo = 'https://s3.amazonaws.com/37assets/svn/1065-IMG_2529.jpg',
+    role = '2';
--- resources/sql/fixtures_extra.sql
+++ resources/sql/fixtures_extra.sql
@@ -26,6 +26,7 @@ INSERT INTO thread
        FROM user
        WHERE username = 'admin'),
     idcategory = @category1;
+SET @thread2 = LAST_INSERT_ID();
 
 INSERT INTO thread
     SET title = 'Troisieme thread',
@@ -34,6 +35,25 @@ INSERT INTO thread
        FROM user
        WHERE username = 'admin'),
     idcategory = @category1;
+SET @thread3 = LAST_INSERT_ID();
+
+INSERT INTO thread
+    SET title = 'Premier thread dans la categorie 2',
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'admin'),
+    idcategory = @category2;
+SET @thread4 = LAST_INSERT_ID();
+
+INSERT INTO thread
+    SET title = 'Premier thread dans la categorie 3',
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'admin'),
+    idcategory = @category3;
+SET @thread5 = LAST_INSERT_ID();
 
 SET @ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
@@ -51,7 +71,7 @@ INSERT INTO post
     iduser = (
        SELECT iduser
        FROM user
-       WHERE username = 'admin'),
+       WHERE username = 'benoit'),
     creationdate = NOW(),
     idthread = @thread1;
 
@@ -60,6 +80,42 @@ INSERT INTO post
     iduser = (
        SELECT iduser
        FROM user
-       WHERE username = 'admin'),
+       WHERE username = 'toto'),
     creationdate = NOW(),
     idthread = @thread1;
+
+INSERT INTO post
+    SET message = @ipsum,
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'toto'),
+    creationdate = NOW(),
+    idthread = @thread2;
+
+INSERT INTO post
+    SET message = @ipsum,
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'benoit'),
+    creationdate = NOW(),
+    idthread = @thread3;
+
+INSERT INTO post
+    SET message = @ipsum,
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'toto'),
+    creationdate = NOW(),
+    idthread = @thread4;
+
+INSERT INTO post
+    SET message = @ipsum,
+    iduser = (
+       SELECT iduser
+       FROM user
+       WHERE username = 'admin'),
+    creationdate = NOW(),
+    idthread = @thread5;
